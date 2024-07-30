edaef0b1f8e6c7f25c854998dda84101711af8d3
lamaAbu <103049368+lamaAbu@users.noreply.github.com>
2024-07-30 19:51:18 +0300

    alpha beta is done

--- submission.py
+++ submission.py
@@ -1,11 +1,14 @@
 from Agent import Agent, AgentGreedy
 from WarehouseEnv import WarehouseEnv, manhattan_distance
 import random
+import numpy
 
 #added by Lama
 from WarehouseEnv import board_size
 import time
 
+
+MAX_D = 10
 
 
 # TODO: section a : 3
@@ -35,8 +38,9 @@ def smart_heuristic(env: WarehouseEnv, robot_id: int):
 
         dist_to_package = manhattan_distance(cur_robot.position,best_package.position)
         dist_to_destination = manhattan_distance(best_package.destination,best_package.position)
-        if (dist_to_package + dist_to_destination) > min(env.num_steps, cur_robot.battery):
-           # if env.num_steps >= 
+        
+        if (dist_to_package + dist_to_destination) > min(env.num_steps, cur_robot.battery) and len(packages_on_board) == 2:
+            best_package = packages_on_board[1 - packages_on_board.index(best_package)]
         return (10000*(credit + 100) + 1/(100*(1 + manhattan_distance(best_package.position,cur_robot.position))))
 
 
@@ -57,17 +61,17 @@ def heuristic_for_minimax(env: WarehouseEnv, agent_id):
     Jad = env.get_robot(1 - agent_id)
 
     if Lama.credit < Jad.credit:
-        return float('-inf')
+        return -numpy.inf
     
     elif Lama.credit > Jad.credit:
-        return float('inf')
+        return numpy.inf
     
-    return 0 #?
+    return 0
 
 
 def time_out_ERROR(start_t, limit_t):
     cur_t = time.time()
-    epsilon = 5e-2
+    epsilon = 0.2
     if start_t + limit_t  < cur_t + epsilon:
         #time limit reached
         raise RuntimeError
@@ -81,31 +85,10 @@ def time_out(start_t, limit_t):
 
 class AgentMinimax(Agent):
 
-    def max_succ(self, env: WarehouseEnv, me_robot, cur_robot, cur_depth, limit_time, start_time):
-        max_val = float('-inf')
-        operators = env.get_legal_operators(cur_robot)
-        children = [env.clone() for _ in operators]
-        for child, op in zip(children, operators):
-            child.apply_operator(cur_robot, op)
+    def __init__(self):
+        super().__init__()
+        self.op = None
 
-        for _, child in zip(operators, children):
-            tmp = self.minimax(child, me_robot, cur_robot, cur_depth, limit_time, start_time)
-            if tmp > max_val:
-                max_val = tmp
-        return max_val
-
-    def min_succ(self, env: WarehouseEnv, me_robot, cur_robot, cur_depth, limit_time, start_time):
-        min_val = float('inf')
-        operators = env.get_legal_operators(cur_robot)
-        children = [env.clone() for _ in operators]
-        for child, op in zip(children, operators):
-            child.apply_operator(cur_robot, op)
-
-        for _, child in zip(operators, children):
-            tmp = self.minimax(child, me_robot, cur_robot, cur_depth, limit_time, start_time)
-            if tmp < min_val:
-                min_val = tmp
-        return min_val
     
     def minimax(self, env: WarehouseEnv, me_robot, cur_robot, cur_depth, limit_time, start_time):
         time_out_ERROR(start_time, limit_time)
@@ -116,10 +99,33 @@ class AgentMinimax(Agent):
         
         #if the next agent is MAX: return max-value(state)
         if me_robot == cur_robot:
-            return self.max_succ(env, me_robot, cur_robot, cur_depth-1, limit_time, start_time)
+            max_val = float('-inf')
+            operators = env.get_legal_operators(cur_robot)
+            children = [env.clone() for _ in operators]
+            for child, op in zip(children, operators):
+                child.apply_operator(cur_robot, op)
+
+            for _, child in zip(operators, children):
+                tmp = self.minimax(child, me_robot,  1 - cur_robot, cur_depth, limit_time, start_time)
+                if tmp > max_val:
+                    max_val = tmp
+            return max_val
+        else:
+            min_val = float('inf')
+            operators = env.get_legal_operators(cur_robot)
+            children = [env.clone() for _ in operators]
+            for child, op in zip(children, operators):
+                child.apply_operator(cur_robot, op)
+
+            for _, child in zip(operators, children):
+                tmp = self.minimax(child, me_robot, 1 - cur_robot, cur_depth, limit_time, start_time)
+                if tmp < min_val:
+                    min_val = tmp
+            return min_val
+            # return self.max_succ(env, me_robot, cur_robot, cur_depth-1, limit_time, start_time)
         
         #if the next agent is MIN: return min-value(state)
-        return self.min_succ(env, me_robot, cur_robot, cur_depth-1, limit_time, start_time)
+        # return self.min_succ(env, me_robot, cur_robot, cur_depth-1, limit_time, start_time)
     
 
     # TODO: section b : 1
@@ -127,29 +133,113 @@ class AgentMinimax(Agent):
         start_time = time.time()
         robot_enemy = 1 - agent_id
         depth = 0 #trying
-        while True:
+        while depth < MAX_D:
             try:
                 operators = env.get_legal_operators(agent_id)
                 children = [env.clone() for _ in operators]
                 for child, op in zip(children, operators):
                     child.apply_operator(agent_id, op)
-
+                
                 children_heuristics = [self.minimax(child, agent_id, robot_enemy, depth, time_limit, start_time) for child in children]
+                
+                for i, child in enumerate(children_heuristics):
+                    if child == None:
+                        children_heuristics.pop(i)
                 depth = depth + 3
                 max_heuristic = max(children_heuristics)
                 index_selected = children_heuristics.index(max_heuristic)
+                self.op = operators[index_selected]
+                
             except RuntimeError:
                 return operators[index_selected]
+        return self.op
 
         
         
 
 
 class AgentAlphaBeta(Agent):
+
+    def __init__(self):
+        super().__init__()
+        self.op = None
+
+    
+    def minimax(self, env: WarehouseEnv, me_robot, cur_robot, cur_depth, limit_time, start_time, alpha, beta):
+        time_out_ERROR(start_time, limit_time)
+        #from lecture
+        #if the state is a terminal state: return the state’s utility
+        if env.done() or cur_depth <= 0: #or check time limit and depth?
+            return heuristic_for_minimax(env, me_robot)
+        
+        #if the next agent is MAX: return max-value(state)
+        if me_robot == cur_robot:
+            max_val = float('-inf')
+            operators = env.get_legal_operators(cur_robot)
+            children = [env.clone() for _ in operators]
+            for child, op in zip(children, operators):
+                child.apply_operator(cur_robot, op)
+
+            for _, child in zip(operators, children):
+                tmp = self.minimax(child, me_robot,  1 - cur_robot, cur_depth, limit_time, start_time, alpha, beta)
+                if tmp > max_val:
+                    max_val = tmp
+                alpha = max(max_val, alpha)
+                if max_val >= beta:
+                    return numpy.inf
+            return max_val
+        
+        else:
+            min_val = float('inf')
+            operators = env.get_legal_operators(cur_robot)
+            children = [env.clone() for _ in operators]
+            for child, op in zip(children, operators):
+                child.apply_operator(cur_robot, op)
+
+            for _, child in zip(operators, children):
+                tmp = self.minimax(child, me_robot, 1 - cur_robot, cur_depth, limit_time, start_time, alpha, beta)
+                if tmp < min_val:
+                    min_val = tmp
+                beta = min(min_val, beta)
+                if min_val <= alpha:
+                    return -numpy.inf
+            return min_val
+            # return self.max_succ(env, me_robot, cur_robot, cur_depth-1, limit_time, start_time)
+        
+        #if the next agent is MIN: return min-value(state)
+        # return self.min_succ(env, me_robot, cur_robot, cur_depth-1, limit_time, start_time)
+    
     # TODO: section c : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        start_time = time.time()
+        robot_enemy = 1 - agent_id
+        depth = 0 #trying
+        while depth < MAX_D:
+            try:
+                operators = env.get_legal_operators(agent_id)
+                children = [env.clone() for _ in operators]
+                for child, op in zip(children, operators):
+                    child.apply_operator(agent_id, op)
+                
+                children_heuristics = [self.minimax(child, agent_id, robot_enemy, depth, time_limit, start_time, -numpy.inf, numpy.inf) for child in children]
+                
+                for i, child in enumerate(children_heuristics):
+                    if child == None:
+                        children_heuristics.pop(i)
+                depth = depth + 3
+                max_heuristic = max(children_heuristics)
+                index_selected = children_heuristics.index(max_heuristic)
+                self.op = operators[index_selected]
+                
+            except RuntimeError:
+                return operators[index_selected]
+        return self.op
 
+    
+    
+    
+
+    
 
 class AgentExpectimax(Agent):
     # TODO: section d : 1
